4034ba1cf5fc844fa1ea539faef3204eb54f3714
MamboBryan <mambobryan@gmail.com>
2026-03-10 20:02:35 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // android
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    // google
+    alias(libs.plugins.google.devtools.ksp) apply false
+    // kotlin
+    alias(libs.plugins.kotlin.compose) apply false
+}
